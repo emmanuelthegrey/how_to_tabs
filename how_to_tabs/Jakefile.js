07fa366e2d5cd125eb36aeb5cfc4cfc34a489019
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-06 23:29:46 -0400

    updating build to check for version

--- how_to_tabs/Jakefile.js
+++ how_to_tabs/Jakefile.js
@@ -1,9 +1,22 @@
 (function() {
 	"use strict";
    
+var EXPECTED_NODE_VERSION = "v10.16.0";
+
     desc("Default build");
-    task("default",function(){
+    task("default",["version"],function(){
         console.log("\n\n Build OK");
     });
+desc("Check Node Version")
+task("version", function(){
+console.log("checking node version");
+
+let actualVersion = process.version;
+
+if(actualVersion !== EXPECTED_NODE_VERSION){
+    fail("current version of node :" + actualVersion+ "does not work. Version :" + EXPECTED_NODE_VERSION + "Needed.")
+}
+
+});
 
 }());
