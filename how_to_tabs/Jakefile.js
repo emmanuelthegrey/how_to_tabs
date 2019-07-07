36054431455478b99de52ca2627da13ea60d9db0
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-07 19:31:26 -0400

    Add a iffy template and update jshint options

--- how_to_tabs/Jakefile.js
+++ how_to_tabs/Jakefile.js
@@ -1,7 +1,10 @@
+/*  globals desc:false, task:false, complete:false, fail:false */
+
 (function() {
 	"use strict";
    
 var semver = require("semver");
+var jshint = require("simplebuild-jshint");
 
     desc("Default build");
     task("default",["version", "lint"],function(){
@@ -13,9 +16,39 @@ console.log("checking node version");
 
 desc("Lint code for errors");
 task("lint", function(){
-console.log("Linting JS");
-jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", {interactive:true}, this.complete);
+process.stdout.write("Linting JS");
+//jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", {interactive:true}, this.complete);
+jshint.checkFiles({
+    files: "Jakefile.js",
+    options:{
+        bitwise:true,
+        curly:true,
+        eqeqeq:true,
+        forin:true,
+        freeze:true,
+        futurehostile:true,
+        latedef:"nofunc",
+        nocomma:true,
+        nonbsp:true,
+        nonew:true, 
+        strict:true,
+        undef:true,
+
+
+        node:true,
+        browser:true,
+    },
+    globals:{}
+},complete, fail);
 },{async:true});
+
+function deleteme(){
+    var a = "someString";
+
+    if(typeof a === "string"){
+        console.log("Hi");
+    }
+}
 
 var packageJson = require("./package.json");
 var expectedNodeVersion = packageJson.engines.node;
