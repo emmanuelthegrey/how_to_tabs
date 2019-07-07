5474eee5ffd5f0facb297a0b320e8c16651ebfbd
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-07 19:54:36 -0400

    Set up http-server in Jakefile.js

--- how_to_tabs/Jakefile.js
+++ how_to_tabs/Jakefile.js
@@ -1,65 +1,76 @@
-/*  globals desc:false, task:false, complete:false, fail:false */
+/*  globals jake:false, desc:false, task:false, complete:false, fail:false */
 
-(function() {
-	"use strict";
-   
-var semver = require("semver");
-var jshint = require("simplebuild-jshint");
+(function () {
+    "use strict";
+
+    var semver = require("semver");
+    var jshint = require("simplebuild-jshint");
+
+    //**** General Purpose Tasks
 
     desc("Default build");
-    task("default",["version", "lint"],function(){
+    task("default", ["version", "lint"], function () {
         console.log("\n\n Build OK");
     });
-desc("Check Node Version");
-task("version", function(){
-console.log("checking node version");
 
-desc("Lint code for errors");
-task("lint", function(){
-process.stdout.write("Linting JS");
-//jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", {interactive:true}, this.complete);
-jshint.checkFiles({
-    files: "Jakefile.js",
-    options:{
-        bitwise:true,
-        curly:true,
-        eqeqeq:true,
-        forin:true,
-        freeze:true,
-        futurehostile:true,
-        latedef:"nofunc",
-        nocomma:true,
-        nonbsp:true,
-        nonew:true, 
-        strict:true,
-        undef:true,
+    desc("Run a localhost server");
+    task("run",function(){
+        console.log("Starting local host server");
+        jake.exec("node node_modules/http-server/bin/http-server src", {interactive:true}, complete);
+    },{async:true});
 
+    //**** Supporting Tasks
 
-        node:true,
-        browser:true,
-    },
-    globals:{}
-},complete, fail);
-},{async:true});
-
-function deleteme(){
-    var a = "someString";
-
-    if(typeof a === "string"){
-        console.log("Hi");
-    }
-}
-
-var packageJson = require("./package.json");
-var expectedNodeVersion = packageJson.engines.node;
+    desc("Lint code for errors");
+        task("lint", function () {
+            process.stdout.write("Linting JS");
+            //jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", {interactive:true}, complete);
+            jshint.checkFiles({
+                files: "Jakefile.js",
+                options: {
+                    bitwise: true,
+                    curly: true,
+                    eqeqeq: true,
+                    forin: true,
+                    freeze: true,
+                    futurehostile: true,
+                    latedef: "nofunc",
+                    nocomma: true,
+                    nonbsp: true,
+                    nonew: true,
+                    strict: true,
+                    undef: true,
 
 
-var actualVersion = process.version;
+                    node: true,
+                    browser: true,
+                },
+                globals: {}
+            }, complete, fail);
+        }, { async: true });
 
-if(semver.neq(expectedNodeVersion,actualVersion)){
-    fail("current version of node :" + actualVersion+ "does not work. Version :" + expectedNodeVersion + "Needed.");
-}
+    desc("Check Node Version");
+    task("version", function () {
+        console.log("checking node version");
 
-});
+        // function deleteme(){
+        //     var a = "someString";
+
+        //     if(typeof a === "string"){
+        //         console.log("Hi");
+        //     }
+        // }
+
+        var packageJson = require("./package.json");
+        var expectedNodeVersion = packageJson.engines.node;
+
+
+        var actualVersion = process.version;
+
+        if (semver.neq(expectedNodeVersion, actualVersion)) {
+            fail("current version of node :" + actualVersion + "does not work. Version :" + expectedNodeVersion + "Needed.");
+        }
+
+    });
 
 }());
