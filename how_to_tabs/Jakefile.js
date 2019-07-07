443a8795b630e93010a6d05161f2fb0ad6b8bab3
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-07 14:22:47 -0400

    Automatic linting setup

--- how_to_tabs/Jakefile.js
+++ how_to_tabs/Jakefile.js
@@ -4,21 +4,27 @@
 var semver = require("semver");
 
     desc("Default build");
-    task("default",["version"],function(){
+    task("default",["version", "lint"],function(){
         console.log("\n\n Build OK");
     });
-desc("Check Node Version")
+desc("Check Node Version");
 task("version", function(){
 console.log("checking node version");
 
-var packageJson = require("./package.json")
+desc("Lint code for errors");
+task("lint", function(){
+console.log("Linting JS");
+jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", {interactive:true}, this.complete);
+},{async:true});
+
+var packageJson = require("./package.json");
 var expectedNodeVersion = packageJson.engines.node;
 
 
-let actualVersion = process.version;
+var actualVersion = process.version;
 
 if(semver.neq(expectedNodeVersion,actualVersion)){
-    fail("current version of node :" + actualVersion+ "does not work. Version :" + expectedNodeVersion + "Needed.")
+    fail("current version of node :" + actualVersion+ "does not work. Version :" + expectedNodeVersion + "Needed.");
 }
 
 });
