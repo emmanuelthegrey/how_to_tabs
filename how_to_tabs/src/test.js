9d40c8cb1a8e539c674ad60b783e678823b924d4
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-07 22:40:41 -0400

    Example of automated test assertion

--- how_to_tabs/src/test.js
+++ how_to_tabs/src/test.js
@@ -1,3 +1,14 @@
 (function(){
   
+
+function assertEqual(actual, expected){
+    if(actual !== expected) throw new Error("Expected: " + expected + " does not equal Actual: " + actual);
+}
+
+function add(a, b){
+    return a + b;
+}
+
+assertEqual(add(1,2),4);
+
   }());
