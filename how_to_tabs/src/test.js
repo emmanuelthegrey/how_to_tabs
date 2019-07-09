bf96062b9c6428145f871d9d5650d3d5769d4501
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-09 08:51:44 -0400

    Floating point test not working

--- how_to_tabs/src/test.js
+++ how_to_tabs/src/test.js
@@ -1,5 +1,8 @@
 (function(){
-  
+  var assert = require("chai").assert;
+
+assert.equal(add(1,2),3);
+assert.equal(add(.1,.2),.3);
 
 function assertEqual(actual, expected){
     if(actual !== expected) throw new Error("Expected: " + expected + " does not equal Actual: " + actual);
@@ -9,6 +12,6 @@ function add(a, b){
     return a + b;
 }
 
-assertEqual(add(1,2),4);
+//assertEqual(add(1,2),4);
 
   }());
