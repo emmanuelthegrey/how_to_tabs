ac9bca18927c9336a16a8b10f331d25294a3e225
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-09 16:30:08 -0400

    Updated tests with it and describe statments

--- how_to_tabs/src/test.js
+++ how_to_tabs/src/test.js
@@ -1,14 +1,27 @@
 (function(){
   var assert = require("chai").assert;
+describe("Addition",function(){
 
-assert.equal(add(1,2),3);
-assert.equal(add(.1,.2),.3);
+    it("Adds two numbers",function(){
+        assert.equal(add(1,2),3);
+    })
+
+it("Adds two numbers where at least one contains floating point",function(){
+    assert.equal(add(.1,.2),.3);
+
+})
+})
+
 
 function assertEqual(actual, expected){
     if(actual !== expected) throw new Error("Expected: " + expected + " does not equal Actual: " + actual);
 }
 
 function add(a, b){
+
+    if(a % 1 !== 0 || b % 1 !== 0){
+        return Math.round((a + b) * 100) / 100 ;
+    }
     return a + b;
 }
 
