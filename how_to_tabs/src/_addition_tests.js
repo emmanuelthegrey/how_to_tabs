005c5f66fe54d7c2b7d9de7ac07b587a7db6ec0f
Emmanuel Aubrey <emmanuelaubrey@emmanuels-mbp.lan>
2019-07-13 17:05:43 -0400

    Created Modules for example test

---
+++ how_to_tabs/src/_addition_tests.js
@@ -0,0 +1,16 @@
+(function(){
+    "use strict";
+var addition = require("./addition.js");
+  var assert = require("chai").assert;
+describe("Addition",function(){
+
+    it("Adds two numbers",function(){
+        assert.equal(addition.add(1,2),3);
+    });
+
+it("Adds two numbers where at least one contains floating point",function(){
+    assert.equal(addition.add(0.1,0.2),0.3);
+
+});
+});
+  }());
---
+++ how_to_tabs/src/addition.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+
+    exports.add = function add(a, b){
+
+    if(a % 1 !== 0 || b % 1 !== 0){
+        return Math.round((a + b) * 100) / 100;
+    }
+    return a + b;
+}
+  }());
