b492917725b2f78ad28fa65f0deee9001446e437
Emmanuel Aubrey <emmanuelthegrey@gmail.com>
2019-07-21 15:46:45 -0400

    removed test code

--- how_to_tabs/src/javascript/_addition_tests.js
+++ how_to_tabs/src/javascript/_addition_tests.js
@@ -1,30 +1,10 @@
 (function(){
     "use strict";
-var arithmetic = require("./arithmetic.js");
   var assert = require("./assert.js");
-describe("Addition",function(){
-
-    it("Adds two numbers",function(){
-        assert.equal(arithmetic.add(1,2),3);
-    });
-
-it("Adds two numbers where at least one contains floating point",function(){
-    assert.equal(arithmetic.add(0.1,0.2),0.3);
+describe("Something",function(){
+it("something", function(){
 
 });
 });
 
-describe("Subtraction", function(){
-    it("subtracts positive numbers", function(){
-        var expectedResult = 1;
-       var actualResult = arithmetic.subtract(2,1);
-       assert.equal(actualResult, expectedResult);
-    });
-});
-
-describe("Multiplication", function(){
-it("Multiple two numbers", function(){
-   assert.equal(arithmetic.multiply(2,2), 4);
-});
-});
   }());
--- how_to_tabs/src/javascript/app.js
+++ how_to_tabs/src/javascript/app.js
@@ -1,3 +1,2 @@
-var add = require("./arithmetic.js");
 
-console.log("10 + 7 = " + add.add(10,7));
+
