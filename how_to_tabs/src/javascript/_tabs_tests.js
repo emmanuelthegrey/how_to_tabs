2507573d19cb064958eea5c5d7754eee478faf7b
Emmanuel Aubrey <emmanuelthegrey@gmail.com>
2019-07-21 19:06:30 -0400

    Tab skeleton created

---
+++ how_to_tabs/src/javascript/_tabs_tests.js
@@ -0,0 +1,18 @@
+(function () {
+    "use strict";
+    var assert = require("./assert.js");
+    var tabs = require("./tabs.js");
+    describe("Tabs", function () {
+        it("Has an API", function () {
+            tabs.initialize();
+            // var div = document.createElement("div");
+            // div.innerHTML = "I'm a pretty cool div, I just go here";
+            // document.body.appendChild(div);
+            // var p = document.createElement("p");
+            // p.innerHTML = "A graphic representation of data abstracted from the Chinese program’s thrust...";
+            // div.appendChild(p);
+            //  div.parentNode.removeChild(div);
+        });
+    });
+
+}());
